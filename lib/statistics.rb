85795ba0cbcc1f57e5c8dda241aeb28beae54cba
Katie Hoffman <ktahoffman@gmail.com>
2015-03-19 09:58:51 -0400

    add solution

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,1 +1,21 @@
-# your code here
+class Array
+
+  def mean
+    sum = self.inject(:+)
+    (sum.to_f / self.length).round(2)
+  end
+
+  def median
+    index = self.length / 2
+    self.sort[index]
+  end
+
+  def mode
+    counts = {}
+    self.each do |n|
+      counts[n] ||= 0
+      counts[n] += 1
+    end
+    counts.sort_by {|_,v| v}.last.first
+  end
+end
