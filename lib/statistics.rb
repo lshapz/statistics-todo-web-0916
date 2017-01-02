1d8ca22c5c2a762e8f77c6902c14d474fc97760c
lshapz <laurashapiro01@gmail.com>
2017-01-02 15:02:39 -0500

    Done.

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,3 +1,5 @@
+
+require 'byebug' 
 # your code here
 class Array
 
@@ -9,6 +11,22 @@ class Array
     self.sort!
     mid = self.count / 2 
     self[mid]
+  end
+  
+  def mode
+    max_hash = self.each_with_object(Hash.new(0)) do |number, hash|
+      hash[number] += 1
+    end
+
+    count = max_hash.values.max 
+    num = 0
+    max_hash.each do |k, v|
+      if v == count
+        num = k
+        break
+      end
+    end
+    num
   end
 
 end 
